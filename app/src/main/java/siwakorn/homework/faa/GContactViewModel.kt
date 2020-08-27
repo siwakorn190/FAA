d96d27184d9f2824edb9c429471bc237c577bcfc
Siwakorn <demon.evil.30@gmail.com>
2020-08-27 23:27:49 +0700

    Collect and display the data Add code in GContactViewModel

--- app/src/main/java/siwakorn/homework/faa/GContactViewModel.kt
+++ app/src/main/java/siwakorn/homework/faa/GContactViewModel.kt
@@ -1,7 +1,14 @@
 package siwakorn.homework.faa
 
 import android.app.Application
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+import androidx.core.text.HtmlCompat
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Transformations
+import kotlinx.coroutines.*
+import siwakorn.homework.faa.database.Contact
 import siwakorn.homework.faa.database.DatabaseDAO
 import siwakorn.homework.faa.databinding.FragmentContactBinding
 
@@ -10,4 +17,52 @@ class GContactViewModel(
     private val binding: FragmentContactBinding,
     application: Application
 ) : AndroidViewModel(application) {
+
+    private var viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private val contacts = database.get()
+    val contactString = Transformations.map(contacts) { contacts ->
+        formatContact(contacts)
+    }
+
+    private fun formatContact(contact: List<Contact>): Spanned {
+        val sb = StringBuilder()
+        sb.apply {
+            //append(resources.getString(R.string.title))
+            contact.forEach {
+                append(it.id)
+                append(" : ")
+                append(it.name)
+                append(", ")
+                append(it.phone)
+                append("<br>")
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    fun onContactAdd() {
+        uiScope.launch {
+            val newContact = Contact()
+            newContact.name = binding.editTextTextPersonName.text.toString()
+            newContact.phone = binding.editTextTextPersonPhone.text.toString()
+            insert(newContact)
+        }
+    }
+
+    private suspend fun insert(contact: Contact) {
+        withContext(Dispatchers.IO) {
+            database.insert(contact)
+        }
+    }
 }
