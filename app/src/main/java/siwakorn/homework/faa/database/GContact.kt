ed1049af31fa81583af419e1adda439a579eefa7
Siwakorn <demon.evil.30@gmail.com>
2020-08-27 23:07:49 +0700

    Annotate the GContact data class

--- app/src/main/java/siwakorn/homework/faa/database/GContact.kt
+++ app/src/main/java/siwakorn/homework/faa/database/GContact.kt
@@ -1,7 +1,15 @@
 package siwakorn.homework.faa.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contact_table")
 data class Contact (
+    @PrimaryKey(autoGenerate = true)
     var id: Long = 0L,
+    @ColumnInfo(name = "name")
     var name: String = "unnamed",
+    @ColumnInfo(name = "phone")
     var phone: String = ""
 )
