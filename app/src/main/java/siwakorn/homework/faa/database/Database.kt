c712d5b3e69a386ad7826feb1eb4707022a1eaa2
Siwakorn <demon.evil.30@gmail.com>
2020-08-27 23:13:46 +0700

    Create the Database class

---
+++ app/src/main/java/siwakorn/homework/faa/database/Database.kt
@@ -0,0 +1,31 @@
+package siwakorn.homework.faa.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Contact::class], version = 1, exportSchema = false)
+abstract class Database : RoomDatabase(){
+    abstract val databaseDao: DatabaseDAO
+    companion object{
+        @Volatile
+        private var INSTANCE: siwakorn.homework.faa.database.Database? = null
+        fun getInstance(context: Context): siwakorn.homework.faa.database.Database {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        siwakorn.homework.faa.database.Database::class.java,
+                        "database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
