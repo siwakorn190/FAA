7bae46c194b6031fa739c3caac7a41aaad3203b5
Siwakorn <demon.evil.30@gmail.com>
2020-08-27 23:15:39 +0700

    Create GContactViewModel

---
+++ app/src/main/java/siwakorn/homework/faa/GContactViewModel.kt
@@ -0,0 +1,13 @@
+package siwakorn.homework.faa
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import siwakorn.homework.faa.database.DatabaseDAO
+import siwakorn.homework.faa.databinding.FragmentContactBinding
+
+class ContactViewModel(
+    val database: DatabaseDAO,
+    private val binding: FragmentContactBinding,
+    application: Application
+) : AndroidViewModel(application) {
+}
